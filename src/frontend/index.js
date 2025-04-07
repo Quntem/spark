e6b1ab8d32364dc05c8353b007cb5cbd831f0895
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2025-04-07 19:11:18 +0100

    Delete src directory

--- src/frontend/index.js
+++
@@ -1,7 +0,0 @@
-sparkutils.load("ui.js")
-sparkutils.load("CollabKitLocal.js")
-
-// setTimeout(() => {
-//     CollabKit.CurrentUser = new Date().toDateString()
-//     CollabKit.JoinRoom("newroom")
-// }, 500);
